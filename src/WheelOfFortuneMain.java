de3e6caec8649c4e5352ec6cadf36c74942815d6
joshvasquezr <59066378+joshvasquezr@users.noreply.github.com>
2024-08-29 11:39:57 -0700

    Rough draft of WheelOfFortuneMain version of the game. It works, but need to add:  - an already guessed function  - penalty system  - Not a Letter Notification  - How To Play Instructions

--- src/WheelOfFortuneMain.java
+++ src/WheelOfFortuneMain.java
@@ -3,6 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class WheelOfFortuneMain {
     public static void main(String[] args) {
@@ -19,10 +20,53 @@ public class WheelOfFortuneMain {
 
         // Get a random phrase from the list
         Random rand = new Random();
-        int r= rand.nextInt(3); // gets 0, 1, or 2
+        int r = rand.nextInt(3); // gets 0, 1, or 2
         String phrase = phraseList.get(r);
         System.out.println(phrase);
 
-    }
+        // Create the Hidden Phrase
+        StringBuilder hiddenPhrase = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+                char ch = phrase.charAt(i);
+                if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+                    hiddenPhrase.append('*');
+                } else {
+                    hiddenPhrase.append(ch);
+                }
+        }
 
+        boolean phraseFound = false;
+        int totalLettersFound = 0;
+        while (!phraseFound) {
+            System.out.println("Hidden Phrase: " + hiddenPhrase);
+
+            // Prompt user to start guessing letter by letter
+            String promptUser = "Enter your guess: ";
+            System.out.println(promptUser);
+            Scanner scanner =  new Scanner(System.in);
+            String userGuess = scanner.next();
+            System.out.println("User Guess: " + userGuess);
+
+            // Check User Guess
+            int lettersFound = 0;
+            for (int i = 0; i < phrase.length(); i++) {
+                char guess = userGuess.charAt(0);
+                guess = Character.toLowerCase(guess);
+                char phraseChar = phrase.charAt(i);
+
+                // if TRUE: update hiddenPhrase, lettersFound, else check if user found the whole phrase
+                if (guess == Character.toLowerCase(phraseChar)) {
+                    lettersFound++;
+                    hiddenPhrase.setCharAt(i, phraseChar);
+                } else if (phrase.equals(hiddenPhrase.toString())) {
+                    phraseFound = true;
+                }
+            }
+            totalLettersFound += lettersFound;
+            System.out.println("Letters Found: " + lettersFound);
+            System.out.println("Total Letters Found: " + totalLettersFound);
+            System.out.println();
+        }
+        System.out.println("Woohoo you won! It only took you " + totalLettersFound + " guesses.");
+    }
 }
