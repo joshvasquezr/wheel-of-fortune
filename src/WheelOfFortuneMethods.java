8eb6c23b6376bf7a13ed0bbe3faa70392f3a0e7a
joshvasquezr <josh.vasquez.ruiz@gmail.com>
2024-08-30 16:56:56 -0700

    test

--- src/WheelOfFortuneMethods.java
+++ src/WheelOfFortuneMethods.java
@@ -1,17 +1,15 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class WheelOfFortuneMain {
-    public static void main(String[] args) {
+public class WheelOfFortuneMethods {
 
-        WheelOfFortuneMain wheelOfFortune = new WheelOfFortuneMain();
+    // Get the phrase from a file of phrases - randomPhrase();
+    public static String randomPhrase() {
 
-        // Get the phrase from a file of phrases - randomPhrase();
         List<String> phraseList=null;
 
         try {
@@ -24,40 +22,53 @@ public class WheelOfFortuneMain {
         Random rand = new Random();
         int r = rand.nextInt(phraseList.size()); // gets 0, 1, or 2
         String phrase = phraseList.get(r);
-        System.out.println(phrase);
+        return phrase;
+    }
 
-        // Create the Hidden Phrase - generateHiddenPhrase()
-        StringBuilder hiddenPhrase = new StringBuilder();
-        for (int i = 0; i < phrase.length(); i++) {
-                char ch = phrase.charAt(i);
-                if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
-                    hiddenPhrase.append('*');
-                } else {
-                    hiddenPhrase.append(ch);
-                }
-        }
-
+    public static void instructions() {
         // Greeting and Instructions
         String greeting = "WELCOME TO...%nTHE WHEEL OF FORTUNE! %n%n";
         String instructions = "HOW TO PLAY%n______________%n%nYou will be shown a Hidden Phrase that you will need to guess %nThe instructions are as follows%n\t1. You may ONLY guess one letter at a time%n\t\ta. If your guess is CORRECT, the place of the letter(s) will be revealed in the Hidden Phrase%n\t\tb. If your guess is INCORRECT, you gain ONE strike. If you gain THREE strikes. Just like in soccer, you are OUT! %n__________________________________________________________________%n%n%n";
         System.out.printf(greeting);
         System.out.printf(instructions);
+    }
+
+    public static StringBuilder generateHiddenPhrase(String phrase) {
+        StringBuilder hiddenPhrase = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+            char ch = phrase.charAt(i);
+            if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+                hiddenPhrase.append('*');
+            } else {
+                hiddenPhrase.append(ch);
+            }
+        }
+        System.out.println(phrase);
+        return hiddenPhrase;
+    }
+
+    public static String getGuess(StringBuilder hiddenPhrase) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.printf("%n%n");
+        System.out.println("Hidden Phrase: " + hiddenPhrase);
+
+        // Get user guess - getGuess();
+        String promptUser = "Enter your guess: ";
+        System.out.printf(promptUser);
+        String userGuess = scanner.nextLine();
+        return userGuess;
+    }
+
+    public static void processGuess(String phrase, StringBuilder hiddenPhrase) {
 
         // Game-Code Starts
         boolean phraseFound = false;
-        int totalLettersFound = 0;
         int lives = 3;
         String guessedLetters = "";
-        Scanner scanner =  new Scanner(System.in);
-        String missedGuesses = "";
-        while (!phraseFound || lives != 0) {
-            System.out.printf("%n%n");
-            System.out.println("Hidden Phrase: " + hiddenPhrase);
 
-            // Get user guess - getGuess();
-            String promptUser = "Enter your guess: ";
-            System.out.printf(promptUser);
-            String userGuess = scanner.nextLine();
+        while (!phraseFound || lives != 0) {
+
+            String userGuess = getGuess(hiddenPhrase);
 
             // Check User Guess - processGuess();
             char guess = Character.toLowerCase(userGuess.charAt(0));
@@ -84,7 +95,6 @@ public class WheelOfFortuneMain {
                 continue;
             }
 
-
             if (guess >= 'a' && guess <='z') {
                 int correctGuess = 0;
 
@@ -101,14 +111,12 @@ public class WheelOfFortuneMain {
                 // Checks if the user a letter NOT FOUND in the Hidden Phrase and takes away ONE life
                 if (correctGuess == 0) {
                     lives -= 1; // PENALTY: Minus One Life for incorrect guess
-                    missedGuesses += guess;
                     if (lives == 0) {
                         System.out.printf("GAME OVER. You ran out of lives :(%nTry Again!%n");
                         System.out.println("The Hidden Phrase was: '" + phrase + "'");
                         break;
                     }
-                    System.out.printf("Incorrect Guess! You now have [ " + lives + " ] LIVES left.%n");
-                    System.out.printf("Wrong Guesses: " + missedGuesses);
+                    System.out.printf("Incorrect Guess! You now have [" + lives + "] left.%n");
                 }
 
                 // Checks if user discovered the full phrase
@@ -124,6 +132,25 @@ public class WheelOfFortuneMain {
             }
 
         } // end of while loop
+    }
 
+    public static void startGame() {
+        instructions();
+        String phrase = randomPhrase();
+        StringBuilder hiddenPhrase = generateHiddenPhrase(phrase);
+        processGuess(phrase, hiddenPhrase);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Would you like to play a game? [ Enter 'y' OR 'n' ]");
+        Scanner scanner = new Scanner(System.in);
+        String playGame = scanner.nextLine();
+        if (playGame.equals("y") || playGame.equals("yes")) {
+            startGame();
+        } else if (playGame.equals("n") || playGame.equals("no")) {
+            System.out.println("Your Loss.");
+        } else {
+            System.out.println("Um what?");
+        }
     }
 }
