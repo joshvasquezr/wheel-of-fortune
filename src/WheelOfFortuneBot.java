c9d5795feb5f1569e45d498e092b5ffd92034483
joshvasquezr <59066378+joshvasquezr@users.noreply.github.com>
2024-09-03 18:53:11 -0700

    Functioning WheelOfFortuneBot.java class

--- src/WheelOfFortuneBot.java
+++ src/WheelOfFortuneBot.java
@@ -1,19 +1,137 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Random;
 
 
-public class WheelOfFortuneBot extends WheelOfFortuneObject{
+public class WheelOfFortuneBot {
 
-    private String choice;
-    @Override
+    // define instance variable
+    private String phrase;
+    private StringBuilder hiddenPhrase;
+    private String guessedLetters;
+    private int lives;
+
+    // create constructor to initialize instance variables
+    public WheelOfFortuneBot() {
+        this.phrase = randomPhrase();
+        this.hiddenPhrase = generateHiddenPhrase();
+        this.guessedLetters = "";
+        this.lives = 3;
+    }
+
+    public String randomPhrase(){
+        List<String> phraseList=null;
+
+        try {
+            phraseList = Files.readAllLines(Paths.get("phrases.txt"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        // Get a random phrase from the list
+        Random rand = new Random();
+        int r = rand.nextInt(phraseList.size()); // gets 0, 1, or 2
+        return phraseList.get(r);
+    }
+
+    public StringBuilder generateHiddenPhrase() {
+        hiddenPhrase = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+            char ch = phrase.charAt(i);
+            if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+                hiddenPhrase.append('*');
+            } else {
+                hiddenPhrase.append(ch);
+            }
+        }
+        System.out.println(phrase);
+        return hiddenPhrase;
+    }
+
     public String getGuess() {
-        // generate alphabet String
+        System.out.printf("%n%n");
+        System.out.println("Hidden Phrase: " + hiddenPhrase);
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int index = new Random().nextInt(alphabet.length());
-        choice = String.valueOf(alphabet.charAt(index));
-        return choice;
+        Random r = new Random();
+        int randNum = r.nextInt(alphabet.length());
+        String userGuess = alphabet.substring(randNum, randNum + 1);
+        System.out.println("Enter your guess: " + userGuess);
+        return userGuess;
+    }
+
+    public void processGuess(char guess) {
+
+        // Keep record of User Guesses
+        if (guess >= 'a' && guess <= 'z') {
+            if (guessedLetters.isEmpty()) {
+                guessedLetters += guess;
+            } else if(guessedLetters.indexOf(guess) != -1) {
+                System.out.println("You've already guessed '" + guess + "'. Please try again.");
+                System.out.println("GUESSED: " + guessedLetters);
+                return;
+            } else {
+                guessedLetters += ", " + guess;
+            }
+            System.out.println("GUESSED: " + guessedLetters);
+        } else {
+            System.out.println("This is not a valid guess. Please try again :)"); // Prompt if user enters non-letter
+            return;
+        }
+
+        int correctGuess = 0;
+
+        for (int i = 0; i < phrase.length(); i++) {
+            char phraseChar = phrase.charAt(i);
+
+            // if TRUE: update hiddenPhrase, lettersFound, else check if user found the whole phrase
+            if (guess == Character.toLowerCase(phraseChar)) {
+                hiddenPhrase.setCharAt(i, phraseChar);
+                correctGuess++;
+            }
+        }
+
+        // Checks if the user a letter NOT FOUND in the Hidden Phrase and takes away ONE life
+        if (correctGuess == 0) {
+            lives -= 1; // PENALTY: Minus One Life for incorrect guess
+            System.out.printf("Incorrect Guess! You now have [" + lives + "] left.%n");
+        }
+
+    }
+
+    // Game-Code Starts
+    public void startGame() {
+
+        boolean phraseFound = false;
+
+        while (!phraseFound) {
+            String userGuess = getGuess();
+
+            // Check User Guess - processGuess();
+            if (userGuess.trim().isEmpty()) {
+                System.out.println("You did not input anything. Please try again.");
+                return;
+            }
+            char guess = Character.toLowerCase(userGuess.charAt(0));
+            processGuess(guess);
+
+            // checks if you won or lost
+            if (phrase.equals(hiddenPhrase.toString())) {
+                phraseFound = true;
+                System.out.println("Woohoo you won!");
+                System.out.printf("You Correctly Found the Phrase: '" + phrase + "'%n");
+                break;
+            } else if (lives == 0) {
+                System.out.printf("GAME OVER. You ran out of lives :(%nTry Again!%n");
+                System.out.println("The Hidden Phrase was: '" + phrase + "'");
+                break;
+            }
+        } // end of while loop
     }
 
     public static void main(String[] args) {
+
         WheelOfFortuneBot game = new WheelOfFortuneBot();
         game.startGame();
     }
